aa98f845c2a8319ab504eeaf6703bb10fad90ba2
user <user@WINDOWS-PF2L7HE>
2025-02-16 17:52:35 +0500

    Added 'Basics.Tanlash bo’limi uchun 3-amaliy vazifa'

--- Kescha/Program.cs
+++ Kescha/Program.cs
@@ -122,7 +122,99 @@ class Program
 
         Console.WriteLine($"Ingliz tilida: {haftaKuniEn}");
         #endregion
+
+        #region Basics.Tanlash bo’limi uchun 3-amaliy vazifa
+
+        //Task_1 Palindrome Tekshiruvi
+        Console.Write("Satrni kiriting: ");
+        string strPalindrome = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(str);
+
+        string resultPalindrome = isPalindrome ? "Palindrom" : "Palindrom emas";
+        Console.WriteLine(result);
+
+        //Task_2 Temperatura o'girish
+        Console.Write("Selsiyus bo'yicha temperaturani kiriting: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double celsius))
+        {
+            double fahrenheit = celsius * 9 / 5 + 32;
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine($"Selsiyus: {celsius}, Fahrenheit: {fahrenheit:F1}, Kelvin: {kelvin:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Noto'g'ri kirish. Iltimos, son kiriting.");
+        }
+
+        //Task_3 Sonlarni tartiblash
+        Console.Write("Sonlarni vergul bilan ajratib kiriting: ");
+        string inputSort = Console.ReadLine();
+        int[] numbers = input.Split(',').Select(int.Parse).ToArray();
+
+        Console.Write("Tartiblash tartibini kiriting (1 - O'suvchi, 2 - Kamayuvchi): ");
+        string tartib = Console.ReadLine();
+
+        switch (tartib)
+        {
+            case "1":
+                Array.Sort(numbers);
+                break;
+            case "2":
+                Array.Sort(numbers);
+                Array.Reverse(numbers);
+                break;
+            default:
+                Console.WriteLine("Noto'g'ri tartib tanlandi.");
+                return;
+        }
+
+        string resultSort = string.Join(", ", numbers);
+        Console.WriteLine($"Natija: {resultSort}");
+
+        //Task_4 Berilgan matnning simvol sanasini hisoblash
+        Console.Write("Matnni kiriting: ");
+        string inputCountChar = Console.ReadLine();
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in inputCountChar)
+        {
+            if (charCount.ContainsKey(c))
+            {
+                charCount[c]++;
+            }
+            else
+            {
+                charCount[c] = 1;
+            }
+        }
+
+        Console.WriteLine("Simvollar qaytarilishi:");
+        foreach (var item in charCount)
+        {
+            Console.WriteLine($"'{item.Key}': {item.Value} martta");
+        }
+
+        #endregion
+
     }
 
+    #region Basics.Tanlash bo’limi uchun 3-amaliy vazifa
+
+    //Task_1 Palindrome Tekshiruvi
+    static bool IsPalindrome(string str)
+    {
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+        string reverseStr = new string(arr);
+
+        return str.Equals(reverseStr, StringComparison.OrdinalIgnoreCase);
+    }
+
+    #endregion
 }
 
