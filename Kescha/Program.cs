e5b2d5792b1a7bcc4fc41124d69d9430951dd52c
user <user@WINDOWS-PF2L7HE>
2025-02-17 00:55:35 +0500

    Added "Basics.Takrorlash bo’limi uchun 1-amaliy vazifa"

--- Kescha/Program.cs
+++ Kescha/Program.cs
@@ -201,6 +201,61 @@ class Program
 
         #endregion
 
+        #region Basics.Takrorlash bo’limi uchun 1-amaliy vazifa
+
+        //Task_1 1 dan 1000 gacha bo’lgan toq sonlarni yig’indisini ekranga
+            //chiqaradigan dastur tuzing. While loop dan foydalaning!
+        int sum = 0;
+        int number = 1;
+
+        while (number <= 1000)
+        {
+            if (number % 2 != 0) 
+            {
+                sum += number;
+            }
+            number++;
+        }
+
+        Console.WriteLine($"1 dan 1000 gacha bo'lgan toq sonlarning yig'indisi: {sum}");
+
+        //Task_2 Array ichidagi eng katta va eng kichik qiymatlarni ko’paytmasini
+            //ekranga chiqaradigan dastur tuzing. For loop dan foydalaning!
+
+        int[] numbersToplam = { 3, 5, 7, 2, 8, -1, 4, 10, 12 };
+        int engKatta = numbersToplam[0];
+        int engKichik = numbersToplam[0];
+
+        for (int i = 1; i < numbersToplam.Length; i++)
+        {
+            if (numbersToplam[i] > engKatta)
+            {
+                engKatta = numbersToplam[i];
+            }
+            if (numbersToplam[i] < engKichik)
+            {
+                engKichik = numbersToplam[i];
+            }
+        }
+
+        int kopaytma = engKatta * engKichik;
+
+        Console.WriteLine($"Array ichidagi eng katta qiymat: {engKatta}");
+        Console.WriteLine($"Array ichidagi eng kichik qiymat: {engKichik}");
+        Console.WriteLine($"Eng katta va eng kichik qiymatlarni ko'paytmasi: {kopaytma}");
+
+        //Task_3 Consolga int tipidagi butun son kiritilgan => int x
+            //x!(x factorial) ni hisoblaydigan dastur tuzing.
+        Console.Write("Butun sonni kiriting: ");
+        int xFact = int.Parse(Console.ReadLine());
+
+        long factorial = CalculateFactorial(x);
+
+        Console.WriteLine($"{xFact}! = {factorial}");
+
+
+        #endregion
+
     }
 
     #region Basics.Tanlash bo’limi uchun 3-amaliy vazifa
@@ -215,6 +270,22 @@ class Program
         return str.Equals(reverseStr, StringComparison.OrdinalIgnoreCase);
     }
 
+    #endregion
+
+    #region Basics.Takrorlash bo’limi uchun 1-amaliy vazifa
+
+    //Task_3 Factorial funksiasi
+    static long CalculateFactorial(int n)
+    {
+        long result = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
     #endregion
 }
 
